f382270d6daa19e510626e7c0f8a5b99c8566726
usrdno <usrdno@114389e0-4ce9-46a4-9d05-ca277a482ead>
2012-06-18 09:59:19 +0000

    Added BgUtil class in se.notima.bg for backwards compatibility.

---
+++ trunk/bankgiro-api/src/main/java/se/notima/bg/BgUtil.java
@@ -0,0 +1,12 @@
+package se.notima.bg;
+
+/**
+ * Kept for backwards compatibility
+ * 
+ * @author daniel.tamm
+ * @deprecated Use org.notima.bg.BgUtil
+ */
+
+public class BgUtil extends org.notima.bg.BgUtil {
+
+}
